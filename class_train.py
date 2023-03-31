2d5e7110a5b0cc70079f5ac97f72b131be5ad501
RuslanAntonov <102322592+RuslanAntonov@users.noreply.github.com>
2023-03-31 20:26:52 +0300

    Обновление классификатора
    Рабочий классификатор шума с точностью ~0.95

--- class_train.py
+++ class_train.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Flatten, Dropout, LSTM
 
 X_train, \
 Y_train, \
@@ -21,18 +21,19 @@ print("Y_val shape:", Y_val.shape)
 
 print('Build model')
 classifier = Sequential()
-#First Hidden Layer
-classifier.add(Dense(4, activation='relu', kernel_initializer='random_normal', input_shape=(250, 80)))
-#Second  Hidden Layer
-classifier.add(Dense(4, activation='relu', kernel_initializer='random_normal'))
-#Output Layer
-classifier.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
-#Compiling the neural network
-classifier.compile(optimizer ='adam',loss='binary_crossentropy', metrics =['accuracy'])
+classifier.add(LSTM(units=128, input_shape=(14, 80), return_sequences=True))
+classifier.add(Dropout(0.2))
+classifier.add(LSTM(units=64, return_sequences=True))
+classifier.add(Dropout(0.2))
+classifier.add(Flatten())
+classifier.add(Dense(units=64, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+print(classifier.summary())
 
 print('Train')
-#Fitting the data to the training dataset
-cnnhistory = classifier.fit(X_train,Y_train, batch_size=64, epochs=60, validation_data=(X_val, Y_val))
+cnnhistory = classifier.fit(X_train,Y_train, batch_size=32, epochs=10, validation_data=(X_val, Y_val))
 
 classifier.save('classifier.h5')
 
